808113410aace7a218f2167d9e1bd83f75a045af
ravituduru <tudururavikiran@gmail.com>
2019-10-10 14:59:31 +0530

    sample functions

---
+++ 4-All-about-functions/1.py
@@ -0,0 +1,10 @@
+def min_max(numbers):
+    smallest=largest=numbers[0]
+    for item in numbers:
+        if item>largest:
+            largest=item
+        elif item<smallest:
+            smallest=item
+    return smallest,largest
+
+print(min_max([2,3,4,1,55,23,45]))
