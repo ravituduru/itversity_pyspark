f07a10887b9b88936ff19e719398578a9de39b38
ravituduru <tudururavikiran@gmail.com>
2019-10-10 15:02:58 +0530

    sample functions added the comments in the progrom

--- 4-All-about-functions/1.py
+++ 4-All-about-functions/1.py
@@ -1,3 +1,4 @@
+#finding max and min elements from the list
 def min_max(numbers):
     smallest=largest=numbers[0]
     for item in numbers:
@@ -8,3 +9,4 @@ def min_max(numbers):
     return smallest,largest
 
 print(min_max([2,3,4,1,55,23,45]))
+
